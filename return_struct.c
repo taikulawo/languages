f67fea18e548791b53037c5664f255f938035f38
iamwwc <qaq1362211689@gmail.com>
2022-08-31 09:41:25 +0800

    ignore file without extension

---
+++ return_struct.c
@@ -0,0 +1,20 @@
+#include <malloc.h>
+#include <stdio.h>
+
+typedef struct {
+    void *p;
+    int len;
+}req;
+
+req foo() {
+    req x;
+    x.p = malloc(10);
+    x.len = 1;
+    return x;
+}
+
+int main() {
+    req r = foo();
+    printf("%p", r.p);
+    return 0;
+}
