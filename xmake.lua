ebc13eaa35aa784bc34e02eef589e2eda3fb81ce
iamwwc <qaq1362211689@gmail.com>
2023-11-14 14:12:38 +0800

    1

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,16 @@ target("main")
 
 target("connect")
     -- link pthread library
+    set_languages("c99")
     add_links("pthread")
     set_kind("binary")
     add_files("src/connect.c")
-
+    -- use clang can remove libgcc_s.so.1 dependency
+    -- https://unix.stackexchange.com/questions/1812/what-does-libgcc-s-so-contain
+    set_toolchains("clang")
 target("return_struct")
+    -- https://xmake.io/#/manual/project_target?id=targetset_languages
+    set_languages("cxx17")
+    set_toolchains("clang")
     set_kind("binary")
-    add_files("src/return_struct.c") 
+    add_files("src/return_struct.c")
