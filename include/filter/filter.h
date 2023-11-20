1496c197b86ec6ce221f79b09a61c2a1ce8463ec
taikulawo <qaq1362211689@gmail.com>
2023-11-20 11:05:28 +0800

    1

--- include/filter/filter.h
+++ include/filter/filter.h
@@ -1,7 +1,30 @@
 #include <memory>
+#include <iostream>
 
+namespace NetworkFilters {
 class CodecFactory {
+  public:
     virtual void requestDecoder() = 0;
 };
 
+class CodecFactoryImpl : public CodecFactory {
+  public:
+    CodecFactoryImpl(){};
+
+  private:
+    void requestDecoder() override {
+        std::cout << "request decoder" << std::endl;
+    }
+};
+
 using CodecFactoryPtr = std::unique_ptr<CodecFactory>;
+class Filter {
+  public:
+    Filter(CodecFactoryPtr codec) : codec_factory(std::move(codec)){};
+    void request() { this->codec_factory->requestDecoder(); }
+
+  private:
+    CodecFactoryPtr codec_factory;
+};
+
+} // namespace NetworkFilters
--- src/move.cc
+++ src/move.cc
@@ -5,13 +5,14 @@
 int main() {
     std::vector<int> v{1, 2, 3, 4, 5};
     std::vector<int> w(std::move(v));
+    // UB after move
     v.push_back(6);
     w.push_back(7);
+
+    std::unique_ptr<NetworkFilters::CodecFactory> codec =
+        std::make_unique<NetworkFilters::CodecFactoryImpl>();
+    std::unique_ptr<NetworkFilters::Filter> p =
+        std::make_unique<NetworkFilters::Filter>(std::move(codec));
+    p->request();
     return 0;
 }
-
-class Filter {
-  private:
-    CodecFactoryPtr codec_factory;
-};
-
