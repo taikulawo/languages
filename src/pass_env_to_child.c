2a8f72a3ca2e81eccefdcf2e228a410367c61348
taikulawo <qaq1362211689@gmail.com>
2024-04-27 18:53:06 +0800

    add exec

--- src/pass_env_to_child.c
+++ src/pass_env_to_child.c
@@ -6,6 +6,7 @@
 
 static char *prog = "/bin/bash";
 static char *env[] = {"NAME=TEST", NULL};
+static char *const args[] = {"bash", "-c", "echo $0 && /usr/bin/env", NULL};
 int main() {
     int pipe_fd[2];
     if (pipe(pipe_fd) == -1) {
@@ -23,10 +24,24 @@ int main() {
         close(pipe_fd[0]);
         dup2(pipe_fd[1], STDOUT_FILENO);
 
-        execle(prog, "bash", "-c", "/usr/bin/env", NULL, env);
+        // execle 第二个参数是传给子进程的argv[0]，作为 process name
+        // https://stackoverflow.com/questions/2050961/is-argv0-name-of-executable-an-accepted-standard-or-just-a-common-conventi
+
+        // argv is an array of pointers to strings passed to the new program
+        // as its command-line arguments.  By convention, the first of these
+        // strings (i.e., argv[0]) should contain the filename associated
+        // with the file being executed.  The argv array must be terminated
+        // by a NULL pointer.  (Thus, in the new program, argv[argc] will be
+        // NULL.)
+        execle(prog, "foo", "-c", "echo $0 && /usr/bin/env", NULL, env);
+
+        // 如果使用没有e后缀的exec家族函数，child env继承parent
+        // execvp(prog, args);
+
         // exec 传递 __environ 就能将parent的env原封不同传给child
         // execle(prog, "bash", "-c", "/usr/bin/env", NULL, __environ);
 
+        // execvpe(prog, args, env);
         perror("execle error");
         exit(EXIT_FAILURE);
         // child
