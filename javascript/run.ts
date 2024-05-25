e56170c1b69d1d73413f5053ee7e8c4f3060b386
taikulawo <qaq1362211689@gmail.com>
2024-05-25 14:05:16 +0800

    1

--- javascript/run.ts
+++ javascript/run.ts
@@ -4,6 +4,8 @@
 // curl -fsSL https://deno.land/install.sh | sh
 import { $ } from "npm:zx";
 $.verbose = true;
+// not throw an exception on non-zero exit codes.
+$.nothrow = false
 
 // 以下演示
 import { flattenDeep } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
