208e8a5b60ecec7aa69c50e890d9a9c0ddb5d801
taikulawo <qaq1362211689@gmail.com>
2024-06-26 23:33:04 +0800

    1

--- rust/core/src/bin/non_send_fut.rs
+++ rust/core/src/bin/non_send_fut.rs
@@ -1,10 +1,21 @@
 #[tokio::main]
 async fn main() {
     let s = tokio::task::LocalSet::new();
+    tokio::spawn(async {
+        println!("tokio spawn");
+    })
+    .await.unwrap();
     s.run_until(async {
-        s.spawn_local(async move { println!("hello from spawn local") }).await.unwrap();
-        tokio::task::spawn_local(async move { println!("hello from spawn local") }).await.unwrap();
-        tokio::spawn(async move { println!("hello from spawn Send") }).await.unwrap();
+        s.spawn_local(async move { println!("hello from spawn local") })
+            .await
+            .unwrap();
+        tokio::task::spawn_local(async move { println!("hello from spawn local") })
+            .await
+            .unwrap();
+        tokio::spawn(async move { println!("hello from spawn Send") })
+            .await
+            .unwrap();
         println!("run until");
-    }).await;
+    })
+    .await;
 }
