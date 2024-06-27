ad8038c21bb2111089b0ec32ae35ab48f39176ad
taikulawo <qaq1362211689@gmail.com>
2024-06-27 10:10:13 +0800

    1

---
+++ rust/core/src/bin/async_fn_fut.rs
@@ -0,0 +1,26 @@
+use std::{future::Future, io};
+
+use future_utils::BoxFuture;
+
+#[tokio::main]
+async fn main() {}
+
+struct RustlsStream<T> {
+    inner: T,
+    fut: Option<BoxFuture<'static, ()>>,
+}
+
+impl<T> RustlsStream<T> {
+    async fn foo(&self) {}
+}
+
+impl<T> Future for RustlsStream<T> {
+    type Output = io::Result<()>;
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        todo!()
+    }
+}
--- rust/core/src/bin/non_send_fut.rs
+++ rust/core/src/bin/non_send_fut.rs
@@ -1,21 +1,28 @@
-#[tokio::main]
-async fn main() {
+use tokio::task;
+
+fn main() {
+    let mut builder = tokio::runtime::Builder::new_current_thread();
+    let rt = builder.enable_all().build().unwrap();
     let s = tokio::task::LocalSet::new();
-    tokio::spawn(async {
-        println!("tokio spawn");
-    })
-    .await.unwrap();
-    s.run_until(async {
-        s.spawn_local(async move { println!("hello from spawn local") })
-            .await
-            .unwrap();
-        tokio::task::spawn_local(async move { println!("hello from spawn local") })
-            .await
-            .unwrap();
-        tokio::spawn(async move { println!("hello from spawn Send") })
-            .await
-            .unwrap();
-        println!("run until");
-    })
-    .await;
+    s.block_on(&rt, async move {
+        tokio::spawn(async {
+            println!("tokio spawn");
+        })
+        .await
+        .unwrap();
+        tokio::task::spawn_local(async {
+            task::spawn_local(async move { println!("hello from spawn local") })
+                .await
+                .unwrap();
+            tokio::task::spawn_local(async move { println!("hello from spawn local") })
+                .await
+                .unwrap();
+            tokio::spawn(async move { println!("hello from spawn Send") })
+                .await
+                .unwrap();
+            println!("run until");
+        })
+        .await
+        .unwrap();
+    });
 }
