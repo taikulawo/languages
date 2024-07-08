e7d8c0555b5a1f877c1b7d34cb1b57598a1c92a2
taikulawo <qaq1362211689@gmail.com>
2024-07-08 19:16:15 +0800

    1

--- rust/src/bin/listener_backlog_transfer.rs
+++ rust/src/bin/listener_backlog_transfer.rs
@@ -1,14 +1,46 @@
 use std::{
+    env::{self, Args},
     error::Error,
+    io,
     net::{SocketAddr, TcpListener},
-    os::fd::AsRawFd,
+    os::fd::{AsRawFd, RawFd},
+    process::exit,
     thread::sleep,
     time::Duration,
 };
 
+use libc::fork;
+use nix::{
+    sys::socket::{socket, AddressFamily, SockFlag, SockType},
+    NixPath,
+};
 use socket2::{Domain, SockAddr, Socket, Type};
 
+fn do_upgrade() {}
+
+fn send_fd_to<P>(fd: Vec<RawFd>, payload: &[u8], path: &P) -> io::Result<usize>
+where
+    P: NixPath,
+{
+    let send_fd = socket(
+        AddressFamily::Unix,
+        SockType::Stream,
+        SockFlag::SOCK_NONBLOCK,
+        None,
+    );
+    Ok(0)
+}
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    for ar in args {
+        match &*ar {
+            "--upgrade" => {
+                do_upgrade();
+                return Ok(());
+            }
+            _ => {}
+        }
+    }
     let mut s = Socket::new(Domain::IPV4, Type::STREAM, None)?;
     let bind_addr = "0.0.0.0:14400";
     let addr: SockAddr = bind_addr.parse::<SocketAddr>()?.into();
@@ -27,6 +59,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         sleep(Duration::from_secs(9999999));
     });
     sleep(Duration::from_secs(10));
+    unsafe {
+        let pid = fork();
+        match pid {
+            1.. => {
+                // parent
+            }
+            0 => {
+                // child
+            }
+            _ => {
+                eprintln!("fork error");
+                exit(0)
+            }
+        }
+        if pid == 0 {
+            // child
+        }
+    }
     // fork，将fd发到别的进程
     // let new_sock = socket2::Socket::
     Ok(())
