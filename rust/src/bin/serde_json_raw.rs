844d0dda231ecfe4cdf90ce54a081f8c6ba6263c
taikulawo <qaq1362211689@gmail.com>
2024-08-15 15:19:11 +0800

    1

--- rust/src/bin/serde_json_raw.rs
+++ rust/src/bin/serde_json_raw.rs
@@ -1,6 +1,19 @@
+use serde::{Deserialize, Serialize};
 use serde_json::value::RawValue;
-
+#[derive(Serialize, Deserialize, PartialEq, Debug, Default)]
+struct Block {
+    name: String,
+    age: i32,
+}
 fn main() {
     let n = "60";
     RawValue::from_string(n.to_string()).unwrap();
+    let block = Block {
+        age: 1,
+        name: "haha".into(),
+    };
+    let x = serde_json::to_string(&block).unwrap();
+    let raw_value = RawValue::from_string(x).unwrap();
+    println!("{}", raw_value);
+    serde_json::from_str::<String>("\"1\"").unwrap();
 }
