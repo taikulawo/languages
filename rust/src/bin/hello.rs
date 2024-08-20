d02c2a6a5f347418f8f628bb5952892ea7c836f7
taikulawo <qaq1362211689@gmail.com>
2024-08-20 17:35:51 +0800

    1

--- rust/src/bin/hello.rs
+++ rust/src/bin/hello.rs
@@ -1,3 +1,7 @@
+use url::Url;
+
 fn main() {
+    let u: Url = "https://$backend/$uri?name=$alpn".parse().unwrap();
+    println!("{:?}, {:?} {:?}", u.host(), u.path(), u.query());
     println!("Hello, world!");
 }
