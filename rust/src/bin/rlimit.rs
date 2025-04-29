946982b2b3f8adae051fbc3ad72e1cd142b45062
taikulawo <qaq1362211689@gmail.com>
2025-04-29 12:48:23 +0800

    1

---
+++ rust/src/bin/rlimit.rs
@@ -0,0 +1,17 @@
+fn main() {
+    unsafe {
+        let mut r = libc::rlimit {
+            rlim_cur: 0,
+            rlim_max: 0,
+        };
+        libc::getrlimit(libc::RLIMIT_NOFILE, &mut r);
+        println!("first {:?}", r);
+        let mut r = libc::rlimit {
+            rlim_cur: 1000000,
+            rlim_max: 1000000,
+        };
+        libc::setrlimit(libc::RLIMIT_NOFILE, &r);
+        libc::getrlimit(libc::RLIMIT_NOFILE, &mut r);
+        println!("second {:?}", r);
+    }
+}
